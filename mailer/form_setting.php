36d1b4f24123faa848cd3b80373f29ced2d25895
Parass71 <80063218+Parass71@users.noreply.github.com>
2021-03-04 23:51:51 +0530

    Uploaded Portfolio assets

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New Message on your portfolio'"; //letter subject
+	$to = 'parass71@gmail.com'; // Enter Your E-mail
+	$from = 'paras@hub-creative.com'; // Admin e-mail
+	$fromName = 'Paras portfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
